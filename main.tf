c8e71482a8b49d7a89fb22dea4f864b44d8a77c3
Igor Rodionov <goruha@gmail.com>
2017-08-22 14:46:54 +0600

    Do not run ansible if playbook empty (#5)

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 resource "null_resource" "provisioner" {
+  count = "${signum(length(var.playbook)) == 1 ? 1 : 0}"
   provisioner "local-exec" {
     command = "ansible-playbook ${join(" ", var.arguments)} ${var.playbook} -e ${join("-e", var.envs)}"
   }
