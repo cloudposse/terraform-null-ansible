8975fd6fcefbff764ec1f4377d1b6553f7c8c397
Sergey Vasilyev <s2504s@gmail.com>
2017-08-23 18:34:42 +0300

    added trigger

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
 resource "null_resource" "provisioner" {
   count = "${signum(length(var.playbook)) == 1 ? 1 : 0}"
+
+  triggers {
+    default = "${sha256(file(var.playbook))}"
+
+  }
   provisioner "local-exec" {
     command = "ansible-playbook ${join(" ", var.arguments)} ${var.playbook} -e ${join("-e", var.envs)}"
   }
