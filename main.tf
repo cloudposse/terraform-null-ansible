e9aa92773ec710ba81de8f5d918ceddb970b272e
Sergey Vasilyev <s2504s@gmail.com>
2017-08-24 20:02:06 +0300

    Trigger module execution if a playbook was changed (#6)

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
 resource "null_resource" "provisioner" {
   count = "${signum(length(var.playbook)) == 1 ? 1 : 0}"
+
+  triggers {
+    default = "${sha256(file(var.playbook))}"
+  }
+
   provisioner "local-exec" {
     command = "ansible-playbook ${var.dry_run ? "--check --diff" : ""} ${join(" ", var.arguments)} ${var.playbook} -e ${join("-e", var.envs)}"
   }
