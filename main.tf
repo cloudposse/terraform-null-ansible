43d2e506f04e367af8c548bbbc10f4d39c10f602
SweetOps <vladimir.fess@gmail.com>
2017-09-01 11:43:07 +0300

    renamed trigger default to signature

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ resource "null_resource" "provisioner" {
   count = "${signum(length(var.playbook)) == 1 ? 1 : 0}"
 
   triggers {
-    default = "${sha256(file(var.playbook))}"
-    command = "ansible-playbook ${var.dry_run ? "--check --diff" : ""} ${join(" ", var.arguments)} -e ${join(" -e ", var.envs)} ${var.playbook}"
+    default   = "${sha256(file(var.playbook))}"
+    signature = "ansible-playbook ${var.dry_run ? "--check --diff" : ""} ${join(" ", var.arguments)} -e ${join(" -e ", var.envs)} ${var.playbook}"
   }
 
   provisioner "local-exec" {
