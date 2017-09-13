74b3639087f5fe2e042ecd855aba5dae7590b2a8
Vladimir <vladimir.fess@gmail.com>
2017-09-13 20:41:08 +0300

    Update ansible provisioner triggers (#11)

--- main.tf
+++ main.tf
@@ -1,9 +1,20 @@
+data "archive_file" "default" {
+  type        = "zip"
+  source_dir  = "${dirname(var.playbook)}"
+  output_path = "${path.module}/playbook.zip"
+}
+
 resource "null_resource" "provisioner" {
-  count = "${signum(length(var.playbook)) == 1 ? 1 : 0}"
+  count      = "${signum(length(var.playbook)) == 1 ? 1 : 0}"
+  depends_on = ["data.archive_file.default"]
 
   triggers {
-    signature = "${sha256(dirname(var.playbook))}"
+    signature = "${data.archive_file.default.output_md5}"
     command   = "ansible-playbook ${var.dry_run ? "--check --diff" : ""} ${join(" ", var.arguments)} -e ${join(" -e ", var.envs)} ${var.playbook}"
+  }
+
+  provisioner "local-exec" {
+    command = "rm -f ${data.archive_file.default.output_path}"
   }
 
   provisioner "local-exec" {
