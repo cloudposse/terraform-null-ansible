9bd35e8c2e19249e52500d6796b2b6ec0fd22474
Igor Rodionov <goruha@gmail.com>
2018-01-17 20:55:03 +0600

    Update README (#15)

--- variables.tf
+++ variables.tf
@@ -1,17 +1,21 @@
 variable "arguments" {
   default = []
   type    = "list"
+  description = "Arguments"
 }
 
 variable "envs" {
   default = []
   type    = "list"
+  description = "Environment variables"
 }
 
 variable "playbook" {
   default = ""
+  description = "Playbook to run"
 }
 
 variable "dry_run" {
   default = true
+  description = "Do dry run"
 }
